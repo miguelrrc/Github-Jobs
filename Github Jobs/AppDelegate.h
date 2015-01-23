0426612fae29110460ff053d699a7a5b6c8d3455
Miguel <miguel.rrc@hotmail.es>
2015-01-23 09:57:40 +0100

    Initial Commit

---
+++ Github Jobs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github Jobs
+//
+//  Created by Miguel Rodriguez Rubio on 23/1/15.
+//  Copyright (c) 2015 EPCTracker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github Jobs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github Jobs
+//
+//  Created by Miguel Rodriguez Rubio on 23/1/15.
+//  Copyright (c) 2015 EPCTracker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
